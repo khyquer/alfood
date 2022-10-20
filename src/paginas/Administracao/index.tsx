864bcaa63492c5e52cf7d38dd461b641002c070e
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2022-10-19 21:30:05 -0300

    Listar, editar e excluir

--- src/paginas/Administracao/index.tsx
+++ src/paginas/Administracao/index.tsx
@@ -1,8 +1,19 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import {
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+} from '@mui/material'
 import axios from 'axios'
 
 import { useEffect, useState } from 'react'
 import IRestaurante from '../../interfaces/IRestaurante'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import { Link } from 'react-router-dom'
 
 const AdministracaoRestaurantes = () => {
 	const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
@@ -17,9 +28,15 @@ const AdministracaoRestaurantes = () => {
 			.catch((erro) => {
 				console.log(erro)
 			})
-	}, [])
+	}, [restaurantes])
 
-	
+	const deleteRestaurante = (id: number) => {
+		axios
+			.delete(`http://localhost:8000/api/v2/restaurantes/${id}/`)
+			.catch((erro) => {
+				console.log(erro)
+			})
+	}
 
 	return (
 		<TableContainer component={Paper}>
@@ -28,17 +45,24 @@ const AdministracaoRestaurantes = () => {
 					<TableRow>
 						<TableCell>Id</TableCell>
 						<TableCell>Nome</TableCell>
+						<TableCell>Ações</TableCell>
 					</TableRow>
 				</TableHead>
 				<TableBody>
 					{restaurantes?.map((row) => (
-						<TableRow
-							key={row.id}
-						>
-							<TableCell component="th" scope="row">
+						<TableRow key={row.id}>
+							<TableCell component='th' scope='row'>
 								{row.id}
 							</TableCell>
 							<TableCell>{row.nome}</TableCell>
+							<TableCell>
+								<Link to={`/admin/restaurantes/${row.id}`}>
+									<EditIcon>edit</EditIcon>
+								</Link>
+							</TableCell>
+							<TableCell>
+								<DeleteIcon key={row.id} onClick={()=> deleteRestaurante(row.id)}>edit</DeleteIcon>
+							</TableCell>
 						</TableRow>
 					))}
 				</TableBody>
